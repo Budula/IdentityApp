62e480a160422fb5471258450dcb72eafdb453a3
Budula <williammalheiros_2@hotmail.com>
2022-11-30 20:34:12 -0300

    Configure Identity Migration,

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using IdentityApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("AppDataConnection");
-
+var identityConnectionString = builder.Configuration.GetConnectionString("IdentityConnection");
+//Product Context
 builder.Services.AddDbContext<ProductDbContext>(options =>
     options.UseSqlServer(connectionString));
+//Identity Context
+builder.Services.AddDbContext<IdentityDbContext>(opts =>
+    opts.UseSqlServer(identityConnectionString, options => options.MigrationsAssembly("IdentityApp")));
+//Dependence Injection IEmailSender
+builder.Services.AddScoped<IEmailSender, ConsoleEmailSender>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<IdentityDbContext>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -16,8 +27,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapDefaultControllerRoute();
